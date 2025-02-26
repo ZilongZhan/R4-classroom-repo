8f32017c1958ae514cfbd286371d246c3b93b996
Zilong Zhan <zolong.zhan@gmail.com>
2025-02-26 23:55:40 +0100

    Add getStudentOptions functions to fill student select

--- src/students/service/studentsService.ts
+++ src/students/service/studentsService.ts
@@ -70,13 +70,15 @@ export const deleteStudent = (students: Student[], studentId: number): void => {
   students.splice(indexOfStudentToDelete, 1);
 };
 
-// Crea una función para obtener las opciones de estudiantes para rellenar un select
-// La función debe recibir un array de estudiantes
-// La función debe devolver un array de objetos con tres propiedades: id, name y lastName
-// La propiedad id debe ser el id del estudiante
-// La propiedad name debe ser el nombre del estudiante
-// La propiedad lastName debe ser el apellido del estudiante
-// export const getStudentsOptions =
+export const getStudentsOptions = (
+  students: Student[]
+): { id: number; name: string; lastName: string }[] => {
+  return students.map((student) => ({
+    id: student.id,
+    name: student.name,
+    lastName: student.lastName,
+  }));
+};
 
 // Crea una función para obtener el nombre completo de un estudiante por su id
 // La función debe recibir un array de estudiantes y el id del estudiante
